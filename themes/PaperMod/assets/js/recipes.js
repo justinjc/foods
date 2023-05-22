aa45959145db7e0ba971dd555ec90f2fcc60ce7c
Justin Chan <justin@justinjc.com>
2023-05-22 19:53:03 -0400

    Get gantt data and make divs

--- themes/PaperMod/assets/js/recipes.js
+++ themes/PaperMod/assets/js/recipes.js
@@ -1,16 +1,47 @@
-const gantt = [];
+function getGanttData() {
+  const ganttData = [];
 
-const ganttDomData = document.getElementById('gantt-data');
-for (const item of ganttDomData.children) {
-  gantt.push({
-    id: item.dataset.id,
-    desc: item.dataset.desc,
-    start: item.dataset.start,
-    duration: item.dataset.duration,
-    durationSeconds: item.dataset.durationSeconds,
-    end: item.dataset.end,
-    dependsOn: item.dataset.dependsOn,
-  });
+  const ganttDomData = document.getElementById('gantt-data');
+  if (ganttDomData === null) {
+    return ganttData;
+  }
+
+  for (const item of ganttDomData.children) {
+    ganttData.push({
+      id: item.dataset.id,
+      desc: item.dataset.desc,
+      start: item.dataset.start,
+      duration: item.dataset.duration,
+      durationSeconds: item.dataset.durationSeconds,
+      end: item.dataset.end,
+      dependsOn: item.dataset.dependsOn,
+    });
+  }
+
+  return ganttData;
 }
 
-console.log(gantt);
+function appendGantt() {
+  const ganttDiv = document.getElementById('gantt-container');
+  if (ganttDiv === null) {
+    return;
+  }
+
+  const ganttData = getGanttData();
+  if (ganttData.length === 0) {
+    return;
+  }
+
+  for (const step of ganttData) {
+    const ganttItem = document.createElement('div');
+    ganttItem.classList.add('gantt-item');
+    ganttItem.innerHTML = step.id;
+    // HERE set these correctly
+    let translateX = 40;
+    let translateY = 0;
+    ganttItem.style.transform = `translate(${translateX}px, ${translateY}px)`;
+    ganttDiv.appendChild(ganttItem);
+  }
+}
+
+appendGantt();
